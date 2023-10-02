f3ce42662b56810a279816b7bdb8c59b74148a95
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-10-03 02:19:39 +0300

    task ten

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * check_cycle - check if cycle in the list
+ * @list: an argument
+ * Return: Always 0.
+ */
+int check_cycle(listint_t *list)
+{
+	struct listint_s *ptr1, *ptr2;
+
+	if (!list)
+	return (0);
+
+	ptr1 = list;
+	ptr2 = list->next;
+
+	while (ptr1 && ptr2->next)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+		return (1);
+	}
+return (0);
+
+}
