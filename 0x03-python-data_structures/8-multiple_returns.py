d0cb1a28cfc140ebe666ced7923d1620081d543b
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-10-09 04:19:09 +0300

    task eight

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return
+        return (0, None)
     leng = len(sentence)
     new_tu = (leng, sentence[0])
     return new_tu
