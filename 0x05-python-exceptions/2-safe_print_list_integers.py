19384f29ce36f672cbc5e66f99bc9cd0a59d80f7
root <root@ShihabComputer.localdomain>
2023-10-23 14:21:46 +0300

    task two

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    flag = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except Exception:
+            continue
+        flag = flag + 1
+    print("".format())
+    return (flag)
