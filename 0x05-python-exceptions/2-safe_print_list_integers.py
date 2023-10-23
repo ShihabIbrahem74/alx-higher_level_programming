1c9b30f9b9c46ee790f5ff5a596fecb0a88255de
root <root@ShihabComputer.localdomain>
2023-10-23 14:28:49 +0300

    task two

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,6 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            raise IndexError("list index out of range")
         except Exception:
             continue
         flag = flag + 1
