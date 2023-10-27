1944eb3c724995b3ca14bd5a1d58f3d6c2d04959
root <root@ShihabComputer.localdomain>
2023-10-27 14:33:18 +0200

    task one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, value):
+        """square instance
+
+        Args:
+            value (integer): the size of square
+        """
+        self.__size = value
