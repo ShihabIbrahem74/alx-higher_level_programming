3f322b640108b38258d30eb8e0791eb12e25ee49
root <root@ShihabComputer.localdomain>
2023-10-31 00:05:37 +0200

    task one

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Reactangle Module"""
+
+
+class Rectangle:
+    """creates a Reactangle"""
+
+    def __init__(self, width=0, height=0):
+        """create instances for a rectangle
+
+        Args:
+            width (int, optional): width. Defaults to 0.
+            height (int, optional): hight. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__hight)
+
+    @height.setter
+    def height(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
