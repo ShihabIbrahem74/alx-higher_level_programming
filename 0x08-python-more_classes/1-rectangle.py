73102a50779bec6c16856ae4edddbbda3fc48cca
root <root@ShihabComputer.localdomain>
2023-10-31 00:12:55 +0200

    task one

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,10 +21,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """setter for the private instance attribute width"""
-        if type(value) is not int:
+        if (not isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
