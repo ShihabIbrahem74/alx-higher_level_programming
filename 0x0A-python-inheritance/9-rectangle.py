1429ece5a9e40031fae816471eb952ddd0ce7403
root <root@ShihabComputer.localdomain>
2023-11-07 01:56:27 +0200

    task nine

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module for alx task"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class for alx task
+    """
+    def __init__(self, width, height):
+        """init method for height and width
+
+        Args:
+            width (int): first arguemnt
+            height (int): second argument
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
