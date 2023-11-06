efc40562b205b069f8b6516ccc8a2a22170007ee
root <root@ShihabComputer.localdomain>
2023-11-07 01:26:32 +0200

    task six

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for alx task"""
+
+
+class BaseGeometry:
+    """empty class
+    """
+    def area(self):
+        """for calulation of area
+        """
+        raise Exception("area() is not implemented")
