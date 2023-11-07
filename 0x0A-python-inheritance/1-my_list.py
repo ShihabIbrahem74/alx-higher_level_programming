13af8e7fe14f6d25d228da8f3efe5ddc2a12b66c
root <root@ShihabComputer.localdomain>
2023-11-07 02:14:48 +0200

    task one

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for alx task"""
+
+class MyList(list):
+    """Mylist child class
+
+    Args:
+        list (list): parent class
+    """
+    
+    def print_sorted(self):
+        """method for alx task
+        """
+        print(sorted(self))
+    
+    
