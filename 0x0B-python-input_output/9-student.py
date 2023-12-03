440cc9f9165cf3a1796656688a4c09ee8ee5ddab
root <root@ShihabComputer.localdomain>
2023-12-03 23:42:17 +0200

    task nine

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class Student:
+    """ Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """init
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return "[Student] {} - {} - {:d}".format(self.first_name,
+                                                 self.last_name, self.age)
+
+    def to_json(self):
+        return self.__dict__
