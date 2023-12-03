56ef5a1d50bbd66bcc0e41b787855f25ef1f90dc
root <root@ShihabComputer.localdomain>
2023-12-03 23:16:09 +0200

    task eight

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+""" My class module
+"""
 
-filename = "my_list.json"
-my_list = load_from_json_file(filename)
-print(my_list)
-print(type(my_list))
+class MyClass:
+    """ My class
+    """
 
-filename = "my_dict.json"
-my_dict = load_from_json_file(filename)
-print(my_dict)
-print(type(my_dict))
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
 
-try:
-    filename = "my_set_doesnt_exist.json"
-    my_set = load_from_json_file(filename)
-    print(my_set)
-    print(type(my_set))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    filename = "my_fake.json"
-    my_fake = load_from_json_file(filename)
-    print(my_fake)
-    print(type(my_fake))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
