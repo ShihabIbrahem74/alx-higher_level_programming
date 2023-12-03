d994315cbcc92b2307fc20e1087688d196b332a2
root <root@ShihabComputer.localdomain>
2023-12-03 16:43:38 +0200

    task two

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for alx task"""
+
+
+def write_file(filename="", text=""):
+    """rites a string to a text file (UTF8) and returns the number of characters written
+
+    Args:
+        filename (str): file name. Defaults to "".
+        text (str): date to be written. Defaults to "".
+    """
+    with open(filename, "w") as f:
+        return(f.write(text))
