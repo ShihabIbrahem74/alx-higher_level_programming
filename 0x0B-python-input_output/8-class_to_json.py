0a0e4ff8b000901ef56eea09945056df7937ed9d
root <root@ShihabComputer.localdomain>
2023-12-03 23:16:47 +0200

    task eight

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for alx task"""
 
+
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
@@ -9,5 +10,5 @@ def class_to_json(obj):
     Args:
         obj: instance of a Class
     """
-    
+
     return (obj.__dict__)
