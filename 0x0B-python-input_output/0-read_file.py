3b69aeabc23a67d73b7767fcbdec6ac390341adb
root <root@ShihabComputer.localdomain>
2023-12-03 16:30:07 +0200

    first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for alx task"""
+def read_file(filename=""):
+    """ Reads a text file (UTF8) and prints it to stdout:
+
+    Args:
+        filename (str): file name. Defaults to "".
+    """
+    with open(filename, "r") as f:
+        print(f.read())
